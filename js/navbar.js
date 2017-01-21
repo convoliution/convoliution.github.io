b010752c419481b473500924e839d952609dd8e8
Michael Liu <siphe24@gmail.com>
2017-01-21 09:03:36 -0800

    add some serious navbar wizardry

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,19 @@
 $(function() {
+    var minLiuLeft = parseFloat($(".navbar > .container-fluid").css('padding-left')) + $("#logo > .mi").width();
+    var maxLiuLeft = minLiuLeft + $("#logo > .chael").width() + 6.18;
+    $("#logo > .liu").css('left', maxLiuLeft);
+
+    $(window).scroll(function(){
+        var liuLeft = maxLiuLeft - $(window).scrollTop()/4;
+        if (liuLeft > minLiuLeft) {
+            $("#logo > .liu").css('left', liuLeft);
+            $("#logo > .chael").css('opacity', (liuLeft - minLiuLeft)/(maxLiuLeft - minLiuLeft));
+        } else {
+            $("#logo > .liu").css('left', minLiuLeft);
+            $("#logo > .chael").css('opacity', 0);
+        }
+    })
+
     $(window).mousemove(function(){
         $("#logo").css('visibility', 'visible');
         $("#logo").animate({
