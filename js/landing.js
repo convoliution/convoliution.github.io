ff974b59e9022dab6b54e6cf1fa57335c2cb6603
Michael Liu <siphe24@gmail.com>
2017-01-23 18:48:31 -0800

    reset landing text

--- js/landing.js
+++ js/landing.js
@@ -1,16 +1,3 @@
 $(function() {
-    $("#landing > .liu").css({
-        'left': $("#landing > .mi").width(),
-        'top': $("#landing > .mi").width()
-    });
-    $(window).scroll(function(){
-        $("#landing > .liu").animate({
-            top: 0
-        }, 500);
-        $("#landing > .chael").animate({
-            opacity: 0
-        }, 400, function() {
-            $(this).css('display', 'none');
-        });
-    });
+    
 });
--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 $(function() {
-    var maxLiuLeft = $("#logo > .chael").width() + 6.18;
+    var maxLiuLeft = $("#logo > .chael").width() + 10;
     $("#logo > .liu").css({
         'left': maxLiuLeft
     });
@@ -11,14 +11,17 @@ $(function() {
             $("#logo").css('cursor', 'default')
             $("#logo > .liu").css('left', maxLiuLeft);
             $("#logo > .chael").css('opacity', 1);
+            $("#logo > .chael").css('display', 'inline-block');
         } else if (liuLeft >= 0) {
             $("#logo").css('cursor', 'default')
             $("#logo > .liu").css('left', liuLeft);
             $("#logo > .chael").css('opacity', liuLeft/maxLiuLeft);
+            $("#logo > .chael").css('display', 'inline-block');
         } else {
             $("#logo").css('cursor', 'pointer');
             $("#logo > .liu").css('left', 0);
             $("#logo > .chael").css('opacity', 0);
+            $("#logo > .chael").css('display', 'none');
         }
     });
     $("#logo").click(function() {
