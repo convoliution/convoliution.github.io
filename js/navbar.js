f1ce8410c318b22fcab320ddb35e3d60b8ab89ac
Michael Liu <siphe24@gmail.com>
2017-02-12 10:29:33 -0800

    change navbar to use IDs instead of classes for name

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 $(function() {
-    var maxLiuLeft = $("#logo > .chael").width() + 8;
-    $("#logo > .liu").css({
+    var maxLiuLeft = $("#logo > #chael").width() + 8;
+    $("#logo > #liu").css({
         'left': maxLiuLeft
     });
     var liuLeft;
@@ -9,19 +9,19 @@ $(function() {
         liuLeft = maxLiuLeft + 0 - $(window).scrollTop()/4; // relate scroll distance to "mi liu" distance
         if (liuLeft >= maxLiuLeft) {
             $("#logo").css('cursor', 'default')
-            $("#logo > .liu").css('left', maxLiuLeft);
-            $("#logo > .chael").css('opacity', 1);
-            $("#logo > .chael").css('display', 'inline-block');
+            $("#logo > #liu").css('left', maxLiuLeft);
+            $("#logo > #chael").css('opacity', 1);
+            $("#logo > #chael").css('display', 'inline-block');
         } else if (liuLeft >= 0) {
             $("#logo").css('cursor', 'default')
-            $("#logo > .liu").css('left', liuLeft);
-            $("#logo > .chael").css('opacity', liuLeft/maxLiuLeft);
-            $("#logo > .chael").css('display', 'inline-block');
+            $("#logo > #liu").css('left', liuLeft);
+            $("#logo > #chael").css('opacity', liuLeft/maxLiuLeft);
+            $("#logo > #chael").css('display', 'inline-block');
         } else {
             $("#logo").css('cursor', 'pointer');
-            $("#logo > .liu").css('left', 0);
-            $("#logo > .chael").css('opacity', 0);
-            $("#logo > .chael").css('display', 'none');
+            $("#logo > #liu").css('left', 0);
+            $("#logo > #chael").css('opacity', 0);
+            $("#logo > #chael").css('display', 'none');
         }
     });
     $("#logo").click(function() {
