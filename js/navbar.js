e08547e81b40ae3f79cf949ab90c9b6011120945
Michael Liu <siphe24@gmail.com>
2017-09-03 21:47:23 -0700

    make logo a nav button, permanently
    Previously, the logo only became a clickable nav button after the user scrolls past the landing section.
    I could not find any good reason why this would be the case, and it was inconsistent with the other navigation buttons' behaviors.

--- js/navbar.js
+++ js/navbar.js
@@ -6,24 +6,18 @@ $(function() {
             var liuLeft = minLiuLeft*$(window).scrollTop()/$('#landing').outerHeight();
             var miLeft = maxMiLeft*$(window).scrollTop()/$('#landing').outerHeight();
             if (liuLeft >= 0) {
-                $('#logo').removeClass('nav');
-                $('#logo').css('cursor', 'default');
                 $('#mi').css('left', 0);
                 $('#chael').css('left', 0);
                 $('#liu').css('left', 0);
                 $('#chael').css('opacity', 1);
                 $('#chael').css('display', 'inline-block');
             } else if (liuLeft > minLiuLeft) {
-                $('#logo').removeClass('nav');
-                $('#logo').css('cursor', 'default');
                 $('#mi').css('left', miLeft);
                 $('#chael').css('left', miLeft);
                 $('#liu').css('left', liuLeft);
                 $('#chael').css('opacity', 1 - liuLeft/minLiuLeft);
                 $('#chael').css('display', 'inline-block');
             } else {
-                $('#logo').addClass('nav');
-                $('#logo').css('cursor', 'pointer');
                 $('#mi').css('left', 0); // back to 0 because .chael is display: none
                 $('#liu').css('left', 0); // back to 0 because .chael is display: none
                 $('#chael').css('opacity', 0);
@@ -35,20 +29,14 @@ $(function() {
             var minLiuLeft = -$('#chael').outerWidth();
             var liuLeft = minLiuLeft*$(window).scrollTop()/$('#landing').outerHeight(); // relate scroll distance to 'mi liu' distance
             if (liuLeft >= 0) {
-                $('#logo').removeClass('nav');
-                $('#logo').css('cursor', 'default');
                 $('#liu').css('left', 0);
                 $('#chael').css('opacity', 1);
                 $('#chael').css('display', 'inline-block');
             } else if (liuLeft > minLiuLeft) {
-                $('#logo').removeClass('nav');
-                $('#logo').css('cursor', 'default');
                 $('#liu').css('left', liuLeft);
                 $('#chael').css('opacity', 1 - liuLeft/minLiuLeft);
                 $('#chael').css('display', 'inline-block');
             } else {
-                $('#logo').addClass('nav');
-                $('#logo').css('cursor', 'pointer');
                 $('#liu').css('left', 0); // back to 0 because .chael is display: none
                 $('#chael').css('opacity', 0);
                 $('#chael').css('display', 'none');
