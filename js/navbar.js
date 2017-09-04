a6277851a38c93081b78782cad3a3c63558319fb
Michael Liu <siphe24@gmail.com>
2017-09-04 12:26:09 -0700

    rewrite navbar.js to reuse lines

--- js/navbar.js
+++ js/navbar.js
@@ -1,46 +1,44 @@
 $(function() {
     function updateLiu() {
-        if ($('#logo').css('align-items') === 'normal') { // compact navbar
+        isCompactNavbar = $('#logo').css('align-items') === 'normal';
+        if (isCompactNavbar) {
             var minLiuLeft = -$('#chael').outerWidth()/2;
             var maxMiLeft = $('#chael').outerWidth()/2;
-            var liuLeft = minLiuLeft*$(window).scrollTop()/$('#landing').outerHeight();
             var miLeft = maxMiLeft*$(window).scrollTop()/$('#landing').outerHeight();
+        } else {
+            var minLiuLeft = -$('#chael').outerWidth();
+        }
+        var liuLeft = minLiuLeft*$(window).scrollTop()/$('#landing').outerHeight(); // relate scroll distance to 'mi liu' distance
+
+        // #liu position and #chael opacity
+        if (liuLeft >= 0) {
+            $('#liu').css('left', 0);
+            $('#chael').css('opacity', 1);
+            $('#chael').css('display', 'inline-block');
+        } else if (liuLeft > minLiuLeft) {
+            $('#liu').css('left', liuLeft);
+            $('#chael').css('opacity', 1 - liuLeft/minLiuLeft);
+            $('#chael').css('display', 'inline-block');
+        } else {
+            $('#liu').css('left', 0); // back to 0 because .chael is display: none
+            $('#chael').css('opacity', 0);
+            $('#chael').css('display', 'none');
+        }
+
+        // #mi and #chael positions
+        if (isCompactNavbar) {
             if (liuLeft >= 0) {
                 $('#mi').css('left', 0);
                 $('#chael').css('left', 0);
-                $('#liu').css('left', 0);
-                $('#chael').css('opacity', 1);
-                $('#chael').css('display', 'inline-block');
             } else if (liuLeft > minLiuLeft) {
                 $('#mi').css('left', miLeft);
                 $('#chael').css('left', miLeft);
-                $('#liu').css('left', liuLeft);
-                $('#chael').css('opacity', 1 - liuLeft/minLiuLeft);
-                $('#chael').css('display', 'inline-block');
             } else {
                 $('#mi').css('left', 0); // back to 0 because .chael is display: none
-                $('#liu').css('left', 0); // back to 0 because .chael is display: none
-                $('#chael').css('opacity', 0);
-                $('#chael').css('display', 'none');
             }
-        } else { // normal navbar
-            $('#mi').css('left', 0);
-            $('#chael').css('left', 0);
-            var minLiuLeft = -$('#chael').outerWidth();
-            var liuLeft = minLiuLeft*$(window).scrollTop()/$('#landing').outerHeight(); // relate scroll distance to 'mi liu' distance
-            if (liuLeft >= 0) {
-                $('#liu').css('left', 0);
-                $('#chael').css('opacity', 1);
-                $('#chael').css('display', 'inline-block');
-            } else if (liuLeft > minLiuLeft) {
-                $('#liu').css('left', liuLeft);
-                $('#chael').css('opacity', 1 - liuLeft/minLiuLeft);
-                $('#chael').css('display', 'inline-block');
-            } else {
-                $('#liu').css('left', 0); // back to 0 because .chael is display: none
-                $('#chael').css('opacity', 0);
-                $('#chael').css('display', 'none');
-            }
+        } else {
+            $('#mi').css('left', 0); // reset in case of window resize
+            $('#chael').css('left', 0); // reset in case of window resize
         }
     }
 
