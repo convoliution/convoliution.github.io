82b98a08ad63c4f13d3134c6eef66b9cf1b43cc7
Michael Liu <miliu@protonmail.com>
2025-12-19 15:35:22 -0800

    Implement i fade-in

--- js/index.js
+++ js/index.js
@@ -9,24 +9,57 @@ function easeInOutCubic(x) {
     }
 }
 
-function updateIPosition(progress) {
+function easeInOutSine(x) {
+    x = Math.max(0, x);
+    x = Math.min(x, 1);
+
+    return -(Math.cos(Math.PI * x) - 1) / 2;
+}
+
+function getIStartPosition(word) {
+    return -word.offsetWidth / 3;
+}
+
+function updateIPosition() {
     const word = document.getElementById('word');
     const letterI = document.getElementById('i');
     const letterL = document.getElementById('l');
 
-    const start = - word.offsetWidth / 3;
+    const scrolled = window.scrollY;
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
+    const progress = scrolled / maxScroll;
+
+    const start = getIStartPosition(word);
     const end = letterL.offsetLeft + letterL.offsetWidth;
     const offset = start + (easeInOutCubic(progress) * (end - start));
 
     letterI.style.left = `${offset}px`;
 }
 
-function updateWord() {
-    const scrolled = window.scrollY;
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
-    const progress = Math.min(scrolled / maxScroll, 1);
+function updateIOpacity() {
+    const word = document.getElementById('word');
+    const letterI = document.getElementById('i');
 
-    updateIPosition(progress);
+    const left = getIStartPosition(word);
+    const right = 0;
+    const progress = (letterI.offsetLeft - left) / (right - left);
+
+    letterI.style.opacity = easeInOutSine(progress);
+}
+
+function updateConvolColors() {
+
+}
+
+function updateUtionPosition() {
+
+}
+
+function updateWord() {
+    updateIPosition();
+    updateIOpacity();
+    updateConvolColors();
+    updateUtionPosition();
 }
 
 window.addEventListener('scroll', updateWord);
